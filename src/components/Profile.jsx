734ddb0c9d5a23c957e97d06ad62f34232465e9c
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2020-11-25 01:01:46 +0500

    Lesson 13. Node modules + basic layout

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <main className='profile'>
+      <div>
+        <img src="" alt="#" />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <ul>
+          <li>post 1</li>
+          <li>post 2</li>
+          <li>post 3</li>
+          <li>post 4</li>
+        </ul>
+      </div>
+    </main >
+  )
+}
+
+export default Profile;
