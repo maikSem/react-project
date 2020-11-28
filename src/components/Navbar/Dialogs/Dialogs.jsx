3ec674f6fdabde5fbcb5fdfa58d569d69f86e1be
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2020-11-29 01:31:56 +0500

    Lesson 21. Make page dialogs

--- src/components/Navbar/Dialogs/Dialogs.jsx
+++ src/components/Navbar/Dialogs/Dialogs.jsx
@@ -3,8 +3,19 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={`${classes.dialogs}`}>
+      <ul className={`${classes.dialogsList}`}>
+        <li className={`${classes.dialog}`}>Alesya</li>
+        <li className={`${classes.dialog}`}>Sveta</li>
+        <li className={`${classes.dialog}`}>Oleg</li>
+        <li className={`${classes.dialog}`}>Korney</li>
+        <li className={`${classes.dialog}`}>Dima</li>
+      </ul>
+      <ul className='messages-list'>
+        <li className="message">Hi</li>
+        <li className="message">How are you?</li>
+        <li className="message">Yahy</li>
+      </ul>
     </div>
   )
 }
