b29b29c928f39badbb9ca6ca638880b6128f3aa5
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2020-11-28 19:19:47 +0500

    Lesson 19. Route, brouser-router, npm i react-router-dom -save

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from "./components/Dialogs/Dialogs";
+import Dialogs from './components/Navbar/Dialogs/Dialogs';
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/Navbar/News/News";
+import Music from "./components/Navbar/Music/Music";
+import Settings from "./components/Navbar/Settings/Settings";
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header/>
-      <Navbar/>
-      <div className='app-wrapper-content'>
-        <Dialogs/>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header/>
+        <Navbar/>
+        <div className='app-wrapper-content'>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/dialogs' component={Dialogs}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
+        </div>
       </div>
-      {/*<Profile />*/}
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <ul>
-        <li className={classes.item}>Profile</li>
-        <li className={`${classes.item} ${classes.active}`}>Messages</li>
-        <li>News</li>
-        <li>Music</li>
-        <li>Settings</li>
+        <li className={classes.item}><a href='/profile'>Profile</a></li>
+        <li className={`${classes.item} ${classes.active}`}><a href='/dialogs'>Messages</a></li>
+        <li><a href='/news'>News</a></li>
+        <li><a href='/music'>Music</a></li>
+        <li><a href='/settings'>Settings</a></li>
       </ul>
     </nav>
   )
