3d2ede3e1b53922f25e36d0e4567ead62e4595d8
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2020-11-28 20:12:07 +0500

    Lesson 20. NavLink + active link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <ul>
-        <li className={classes.item}><a href='/profile'>Profile</a></li>
-        <li className={`${classes.item} ${classes.active}`}><a href='/dialogs'>Messages</a></li>
-        <li><a href='/news'>News</a></li>
-        <li><a href='/music'>Music</a></li>
-        <li><a href='/settings'>Settings</a></li>
+        <li className={classes.item}><NavLink activeClassName={classes.activeLink} to='/profile'>Profile</NavLink></li>
+        <li className={`${classes.item}`}><NavLink activeClassName={classes.activeLink} to='/dialogs'>Messages</NavLink></li>
+        <li className={`${classes.item}`}><NavLink activeClassName={classes.activeLink} to='/news'>News</NavLink></li>
+        <li className={`${classes.item}`}><NavLink activeClassName={classes.activeLink} to='/music'>Music</NavLink></li>
+        <li className={`${classes.item}`}><NavLink activeClassName={classes.activeLink} to='/settings'>Settings</NavLink></li>
       </ul>
     </nav>
   )
