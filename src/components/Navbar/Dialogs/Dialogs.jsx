eff9a048faeb699127ed6e1cf5cb37c5d4367f5c
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2020-12-01 23:20:38 +0500

    Lesson 24. UI - BLL (Business Logic Layer). Moved data in array of objects

--- src/components/Navbar/Dialogs/Dialogs.jsx
+++ src/components/Navbar/Dialogs/Dialogs.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+let dialogsData = [
+  {id: 1, name: 'Misha'},
+  {id: 2, name: 'Alesya'},
+  {id: 3, name: 'Ksenya'},
+  {id: 4, name: 'Key'},
+  {id: 5, name: 'Oleg'},
+  {id: 6, name: 'Sergey'}
+]
+
+let messagesData = [
+  {id: 1, message: 'Hi!'},
+  {id: 2, message: 'How are you?'},
+  {id: 3, message: 'Yahoo'},
+  {id: 4, message: 'Yo!'},
+  {id: 5, message: 'Wheare you?'},
+  {id: 6, message: 'Yo!'}
+]
+
 const DialogsItem = (props) => {
   return (
     <div className={classes.dialog}>
@@ -16,23 +34,26 @@ const Message = (props) => {
   )
 }
 
-const Dialogs = () => {
+const Dialogs = (props) => {
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <DialogsItem name='Misha' id='1'/>
-        <DialogsItem name='Alesya' id='2'/>
-        <DialogsItem name='Ksenya' id='3'/>
-        <DialogsItem name='Key' id='4'/>
-        <DialogsItem name='Oleg' id='5'/>
-        <DialogsItem name='Sergey' id='6'/>
+        <DialogsItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+        <DialogsItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+        <DialogsItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+        {/*<DialogsItem name='Alesya' id='2'/>*/}
+        {/*<DialogsItem name='Ksenya' id='3'/>*/}
+        {/*<DialogsItem name='Key' id='4'/>*/}
+        {/*<DialogsItem name='Oleg' id='5'/>*/}
+        {/*<DialogsItem name='Sergey' id='6'/>*/}
       </div>
       <div className='messages-list'>
-       <Message message='Hi!'/>
-       <Message message='How are you?'/>
-       <Message message='Yahoo'/>
-       <Message message='Yo!'/>
-       <Message message='Wheare you?'/>
+        <Message message={messagesData[0].message}/>
+        <Message message={messagesData[1].message}/>
+        {/*<Message message='How are you?'/>*/}
+        {/*<Message message='Yahoo'/>*/}
+        {/*<Message message='Yo!'/>*/}
+        {/*<Message message='Wheare you?'/>*/}
       </div>
     </div>
   )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,15 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+
+  let postData = [
+    {id: 1, post: 'This is my first post!', likesCount: 12},
+    {id: 2, post: 'This is my second post', likesCount: 16},
+    {id: 3, post: 'This is my last post', likesCount: 10}
+  ]
+
+
   return (
     <div className={classes.postsBlock}>
       <h3>My posts</h3>
@@ -15,10 +24,9 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={classes.posts}>
-        <Post message='This is my first post' like='15'/>
-        <Post message='This is my second post' like='12'/>
-        <Post/>
-        <Post/>
+        <Post message={postData[0].post} like={postData[0].likesCount}/>
+        <Post message={postData[1].post} like={postData[1].likesCount}/>
+        <Post message={postData[2].post} like={postData[2].likesCount}/>
       </div>
     </div>
   )
