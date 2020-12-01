a0af11a2aafdf776ca8a5937b564ca7ef8ac4bf2
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2020-12-01 13:03:23 +0500

    Lesson 22. Dialog - NavLink

--- src/components/Navbar/Dialogs/Dialogs.jsx
+++ src/components/Navbar/Dialogs/Dialogs.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
   return (
     <div className={`${classes.dialogs}`}>
-      <ul className={`${classes.dialogsList}`}>
-        <li className={`${classes.dialog}`}>Alesya</li>
-        <li className={`${classes.dialog}`}>Sveta</li>
-        <li className={`${classes.dialog}`}>Oleg</li>
-        <li className={`${classes.dialog}`}>Korney</li>
-        <li className={`${classes.dialog}`}>Dima</li>
-      </ul>
+      <div className={`${classes.dialogsList}`}>
+        <NavLink to='/dialogs/1' className={`${classes.dialog}`}>Alesya</NavLink>
+        <NavLink to='/dialogs/2' className={`${classes.dialog}`}>Sveta</NavLink>
+        <NavLink to='/dialogs/3' className={`${classes.dialog}`}>Oleg</NavLink>
+        <NavLink to='/dialogs/4' className={`${classes.dialog}`}>Korney</NavLink>
+        <NavLink to='/dialogs/5' className={`${classes.dialog}`}>Dima</NavLink>
+      </div>
       <ul className='messages-list'>
         <li className="message">Hi</li>
         <li className="message">How are you?</li>
