9ce4676e864d316f2eba7730e51e0fc1c7101a37
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2020-12-27 22:15:27 +0500

    Lesson 52. Side effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,38 +4,42 @@ import * as axios from "axios";
 import userPhoto from '../../assets/images/userpic.png'
 
 let Users = (props) => {
-  if (props.users.length === 0) {
-   axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-     props.setUsers(response.data.items);
-   });
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        props.setUsers(response.data.items);
+      });
+    }
   }
+
   return (
-    <ul>{
-      props.users.map(u => <li key={u.id}>
-        <div>
-          <div>
-            <img src={u.photos.small || userPhoto} className={classes.userPhoto} alt="#"/>
-          </div>
-          <div>
-            {u.followed
-              ? <button onClick={() => {
-                props.unfollow(u.id)
-              }}>Unfollow</button>
-              : <button onClick={() => {
-                props.follow(u.id)
-              }}>Follow</button>}
-          </div>
-        </div>
-        <div>
-          <span>
-            <div>{u.name}</div>
-            <div>{u.status}</div>
-          </span>
-          <span>
-            <div>{"u.location.country"}</div>
-            <div>{"u.location.city"}</div>
-          </span>
-        </div>
+    <ul>
+      <button onClick={getUsers}>Get users</button>
+      {props.users.map(u => <li key={u.id}>
+      <div>
+      <div>
+      <img src={u.photos.small || userPhoto} className={classes.userPhoto} alt="#"/>
+      </div>
+      <div>
+      {u.followed
+        ? <button onClick={() => {
+          props.unfollow(u.id)
+        }}>Unfollow</button>
+        : <button onClick={() => {
+          props.follow(u.id)
+        }}>Follow</button>}
+      </div>
+      </div>
+      <div>
+      <span>
+      <div>{u.name}</div>
+      <div>{u.status}</div>
+      </span>
+      <span>
+      <div>{"u.location.country"}</div>
+      <div>{"u.location.city"}</div>
+      </span>
+      </div>
       </li>)
     }</ul>
   )
