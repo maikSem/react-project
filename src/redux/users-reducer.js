1930ed494a3f653e1f35e8bc2021fb3e2e34adef
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2021-01-04 01:43:58 +0500

    Lesson 57. Preloader

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,7 +2,8 @@ let FOLLOW = 'FOLLOW';
 let UNFOLLOW = 'UNFOLLOW';
 let SET_USERS = 'SET_USERS';
 let SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
-let SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
+let SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+let TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING'
 
 let initialState = {
   users: [],
@@ -49,6 +50,11 @@ const usersReducer = (state = initialState, action) => {
         ...state, totalUsersCount: action.totalCount
       }
     }
+    case TOGGLE_IS_FETCHING: {
+      return {
+        ...state, isFetching: action.isFetching
+      }
+    }
     default:
       return state;
   }
@@ -59,6 +65,7 @@ export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
 export const setUsersAC = (users) => ({type: SET_USERS, users});
 export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
 export const setTotalUsersCountAC = (totalCount) => ({type: SET_TOTAL_USERS_COUNT, totalCount});
+export const toggleIsFetchingAC = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
 
 
 export default usersReducer;
