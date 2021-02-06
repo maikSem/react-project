977101cbf28eee0a203c3e2245cf0a08241de36b
sma040790@gmail.com <64220364+maikSem@users.noreply.github.com>
2021-02-06 14:18:20 +0500

    Lesson 94. React.lazy, React.Suspense

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {Route, withRouter} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
@@ -14,7 +13,9 @@ import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense";
 
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 
 class App extends React.Component {
 
@@ -25,23 +26,23 @@ class App extends React.Component {
   render() {
 
     if (!this.props.initialized) {
-      return <Preloader />
+      return <Preloader/>
     }
 
     return (
-        <div className='app-wrapper'>
-          <HeaderContainer/>
-          <Navbar/>
-          <div className='app-wrapper-content'>
-            <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-            <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-            <Route path='/users' render={() => <UsersContainer/>}/>
-            <Route path='/login' render={() => <Login/>}/>
-            <Route path='/news' component={News}/>
-            <Route path='/music' component={Music}/>
-            <Route path='/settings' component={Settings}/>
-          </div>
+      <div className='app-wrapper'>
+        <HeaderContainer/>
+        <Navbar/>
+        <div className='app-wrapper-content'>
+          <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+          <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
+          <Route path='/users' render={() => <UsersContainer/>}/>
+          <Route path='/login' render={() => <Login/>}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
         </div>
+      </div>
     );
   }
 }
